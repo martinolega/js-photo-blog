e0b65352c678bbea1ccafb103d1c45124821ce91
martinolega <martinolega11@gmail.com>
2025-11-21 15:26:39 +0100

    feat:feature-complete

--- js/script.js
+++ js/script.js
@@ -3,35 +3,65 @@ const row = document.querySelector(".row");
 const col = document.getElementsByClassName("col");
 const header = document.querySelector("header");
 const body = document.querySelector("body");
-const overlay = document.getElementsByClassName("overlay");
+const overlay = document.getElementById("overlay");
+const button = document.querySelector("button");
+const overlayImage = document.getElementById("overlay-image");
+const image = document.getElementsByClassName("image");
+const pin = document.getElementsByClassName("pin");
 
-let images = [];
+let apiObjects = [];
 //id, title, date, url
-
-
 
 axios
     .get(apiImageUrl)
     .then(function (resp) {
-        images = resp.data;
+        apiObjects = resp.data;
 
         for(let i = 0; i < resp.data.length; i++)
         {
             createCol();
-            const card = createCard(images[i]);
+            const card = createCard(apiObjects[i]);
             printCard(card, col[i]);
-            
-            /*
-            col[i].addEventListener("mouseenter", () => {
-                console.log("Ciao " + images[i].date);
-            });
 
-            col[i].addEventListener("mouseleave", () => {
-                console.log("Addio " + images[i].title);
-            });
-            */
+            insertEventListener(i);
         }
     });
+
+function insertEventListener(i)
+{
+    const image = document.getElementsByClassName("image");
+
+        image[i].addEventListener("click", () => {
+            const overlayClasses = overlay.classList;
+            overlayImage.src = image[i].src;
+            overlayClasses.remove("display-none");
+             overlayClasses.add("flex");
+        });
+
+        col[i].addEventListener("mouseenter", () => {
+            col[i].classList.add("rotate-10");
+            pin[i].classList.add("display-none");
+        });
+
+        col[i].addEventListener("mouseleave", () => {
+            col[i].classList.remove("rotate-10");
+            pin[i].classList.remove("display-none");
+        });
+}
+
+/*
+button.addEventListener("click", () => {
+    const overlayClasses = overlay.classList;
+    overlayClasses.remove("flex");
+    overlayClasses.add("display-none");
+});
+*/
+
+overlay.addEventListener("click", () => {
+    const overlayClasses =overlay.classList;
+    overlayClasses.remove("flex");
+    overlayClasses.add("display-none");
+})
 
 function createCard(activity)
 {
@@ -49,6 +79,7 @@ function createCol()
 {
     let newColumn = document.createElement("div");
     newColumn.classList.add("col");
+
     row.append(newColumn);
 }
 
